c53d6d9ff3f8d7a78c7a0e9625fc7dbe97285186
Emmanuel Odenyire Anyira <eodenyire@gmail.com>
2024-06-04 16:29:37 -0700

    Fixing task 0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -5,15 +5,14 @@
  * binary_tree_node - Creates a binary tree node
  *
  * @parent: Pointer to the parent node
- * @value: Value to be stored in the node
- * Return: Pointer to the newly created node, or NULL on failure
+ * @value: Value to put in the new node
+ *
+ * Return: Pointer to the new node, or NULL on failure
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
-
-    new_node = malloc(sizeof(binary_tree_t));
-    if (new_node == NULL)
+    binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+    if (!new_node)
         return (NULL);
 
     new_node->n = value;
