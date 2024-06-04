4e963d552947cbaf614c094215355ca34e96e35c
Emmanuel Odenyire Anyira <eodenyire@gmail.com>
2024-06-04 16:47:07 -0700

    Our beatiful betty is not happy with me

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,8 +10,10 @@
  * Return: Pointer to the new node, or NULL on failure
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+
 {
 	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+
 	if (!new_node)
 		return (NULL);
 
