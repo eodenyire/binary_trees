257a189a2e6e1eb43d390ed099dd3c97c7a8c262
Void <trigger_austin00@proton.me>
2024-06-06 13:40:27 +0300

    task 17: added check for null parent-> left and parent->right

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -7,18 +7,25 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	if (node == NULL || node->parent == NULL)
+	binary_tree_t *parent;
+
+	if (node == NULL)
+		return (NULL);
+	if (node->parent == NULL)
 		return NULL;
 
-	binary_tree_t *parent = node->parent;
+	parent = node->parent;
 
-	if (parent->left == node)
+	if (parent->left != NULL && parent->right != NULL)
 	{
-		return (parent->right);
-	}
-	else if (parent->right == node)
-	{
-		return (parent->left);
+		if (parent->left == node)
+		{
+			return (parent->right);
+		}
+		else if (parent->right == node)
+		{
+			return (parent->left);
+		}
 	}
 
 	return NULL;
