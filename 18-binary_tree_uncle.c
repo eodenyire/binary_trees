c3657133783a5602ce6d1119731f39be1e3c5f48
Void <trigger_austin00@proton.me>
2024-06-06 13:26:26 +0300

    task 18

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_t - returns sibling to parent of node
+ * @node: node to get the uncle of
+ *
+ * Return: Uncle node or NULL
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	binary_tree_t *parent, *grand_parent;
+
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	parent = node->parent;
+	grand_parent = parent->parent;
+
+	if (grand_parent->left != NULL && grand_parent->right != NULL)
+	{
+		if (grand_parent->left == parent)
+		{
+			return (grand_parent->right);
+		}
+		else if (grand_parent->right == parent)
+		{
+			return (grand_parent->left);
+		}
+	}
+
+	return (NULL);
+}
