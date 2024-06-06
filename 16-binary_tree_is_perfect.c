7409ace2f863c4079a0606b8d034f8f84bd751b4
Void <trigger_austin00@proton.me>
2024-06-06 14:13:59 +0300

    task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -14,17 +14,17 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
+	if (tree->right == NULL)
+		return (0);
+	if (tree->left == NULL)
+		return (0);
 
 	r = binary_tree_is_perfect(tree->right);
 	l = binary_tree_is_perfect(tree->left);
 
 	if (r == l)
 	{
-		if (tree->left == NULL && tree->right == NULL)
-		{
-			return (1);
-		}
-		else if (tree->left != NULL && tree->right != NULL)
+		if (tree->left != NULL && tree->right != NULL)
 		{
 			return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
 		}
