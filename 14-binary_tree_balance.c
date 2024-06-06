4569cafaf08f230829309bc6d5059551660ee209
Void <trigger_austin00@proton.me>
2024-06-06 18:22:45 +0300

    task 14: fixed calculation of height

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,7 +1,10 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_height - find height of tree
+ * @tree: pointer to node
  *
+ * Return: height
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
