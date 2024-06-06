099f75bf14ed4b669f93516df7ccf33d6eae8960
Void <trigger_austin00@proton.me>
2024-06-06 17:58:52 +0300

    task 14: fixed calculation of height

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,22 @@
 #include "binary_trees.h"
 
 /**
+ *
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left, right;
+
+	if (tree == NULL)
+		return (0);
+
+	left = tree->left ? binary_tree_balance(tree->left) + 1 : 0;
+	right = tree->right ? binary_tree_balance(tree->right) + 1 : 0;
+
+	return ((r > l) ? r : l;
+}
+
+/**
  * binary_tree_balance - finds the balance factor of a node
  * Description: bf = height_of_left_subtree - height_of_right_subtree
  * @tree: node to get balance factor off
@@ -14,8 +30,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	r = tree->right ? binary_tree_balance(tree->right) + 1 : 0;
-	l = tree->left ? binary_tree_balance(tree->left) + 1 : 0;
+	r = tree->right ? (int)binary_tree_height(tree->right) + 1 : 0;
+	l = tree->left ? (int)binary_tree_height(tree->left) + 1 : 0;
 
 	BF = l - r;
 
