9395643622bee5258c196e8c3304167d18fc8d13
Void <trigger_austin00@proton.me>
2024-06-06 05:23:41 +0300

    made file executable

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -11,6 +11,9 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 	if (node == NULL)
 		return (0);
 
-	return (node->left == NULL && node->right == NULL) ? 1 : 0;
+	if (node->left == NULL && node->right == NULL) 
+		return (1)
+			
+	return (0);
 }
 
