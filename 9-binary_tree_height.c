7dc066307eef3c52d9019dd2cf293bae8592a393
Void <trigger_austin00@proton.me>
2024-06-06 07:32:24 +0300

    task 9: find height

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - finds the height of a tree
+ * @tree: tree who's height to get
+ *
+ * Return: height of tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	int r, l;
+
+	if (tree == NULL)
+		return (0);
+	r = binary_tree_height(tree->left);
+	l = binary_tree_height(tree->right);
+
+	return ((r > l) ? r + 1 : l + 1);
+}
