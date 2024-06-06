949d903a8a09dedb8b42b6074e30063d73d98306
Void <trigger_austin00@proton.me>
2024-06-06 08:30:58 +0300

    task 11: refactored code to include var size

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -8,13 +8,12 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t r, l;
+	size_t size;
 
 	if (tree == NULL)
 		return (0);
 
-	r = tree->left ? binary_tree_size(tree->left) + 1 : 0;
-	l = tree->right ? binary_tree_size(tree->right) + 1 : 0;
+	size  = (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 
-	return (r + l + 1);
+	return (size);
 }
