b5ef4c07e3d9804b29ad882354b573989c49ecef
Void <trigger_austin00@proton.me>
2024-06-06 06:39:07 +0300

    task 8

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,21 @@
+#include "binary_tree.h"
+
+/**
+ * binary_tree_postorder - traverse tree in post order calling func
+ * on nodes
+ * @tree: tree to traverse
+ * @func: func to call on nodes
+ *
+ * Return: void
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (func == NULL)
+		return;
+	if (tree == NULL)
+		return;
+
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
+}
