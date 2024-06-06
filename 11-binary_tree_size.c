1e34ee2b335b84e69e59a58e0e1ebfadfca5b14b
Void <trigger_austin00@proton.me>
2024-06-06 08:17:35 +0300

    task 11

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - returns the size of a binary tree
+ * @tree: tree to get size of
+ *
+ * Return: size
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	int r, l;
+
+	if (tree == NULL)
+		return (0);
+
+	r = tree->left ? binary_tree_size(tree->left) + 1 : 0;
+	l = tree->right ? binary_tree_size(tree->right) + 1 : 0;
+
+	return (r + l);
+}
