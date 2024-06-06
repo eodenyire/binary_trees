f2ace3e3d49ccf04e29626878334a18a9e66c788
Void <trigger_austin00@proton.me>
2024-06-06 09:52:46 +0300

    task 16

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,35 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_perfect - checks if tree is perfect
+ * Description: considered perfect if it has leaf nodes that are at same level
+ * all nodes have two children and tree is full
+ *
+ * @tree: pointer to root of tree
+ * Return: 0 if not perfect, 1 if perfect
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	int r, l;
+
+	if (tree == NULL)
+		return (0);
+
+	r = binary_tree_is_perfect(tree->right);
+	l = binary_tree_is_perfect(tree->left);
+
+	if (r == l)
+	{
+		if (tree->left == NULL && tree->right == NULL)
+		{
+			return (1);
+		}
+		else if (tree->left != NULL && tree->right != NULL)
+		{
+			return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
+		}
+
+	}
+	
+	return (0);
+}
