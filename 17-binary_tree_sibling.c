975993369fcb3edd16f8913d056f7e67b3ebd63a
Void <trigger_austin00@proton.me>
2024-06-06 11:06:06 +0300

    task 17

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_t - gets the sibling of a node
+ * @node: node to get sibling of
+ * Return: returns a pointer to sibling node
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL)
+		return NULL;
+
+	binary_tree_t *parent = node->parent;
+
+	if (parent->left == node)
+	{
+		return (parent->right);
+	}
+	else if (parent->right == node)
+	{
+		return (parent->left);
+	}
+
+	return NULL;
+}
