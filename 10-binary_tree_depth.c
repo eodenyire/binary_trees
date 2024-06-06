1e032960904f466bdcbbb39b148eafa09b030524
Void <trigger_austin00@proton.me>
2024-06-06 08:04:15 +0300

    task 10

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - to find the depth of tree from node
+ * @tree: node to start from
+ *
+ * Return: depth
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	int depth;
+
+	if (tree = NULL)
+		return (0);
+
+	depth = binary_tree_depth(tree->parent);
+
+	return (depth);
+}
