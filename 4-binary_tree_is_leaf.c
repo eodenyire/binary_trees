a6083f838f41979171101ff5a41e653e300d93a4
Void <trigger_austin00@proton.me>
2024-06-06 05:18:24 +0300

    made file executable

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_leaf - returns true if node is a leaf and 
+ * binary_tree_is_leaf - returns true if node is a leaf and
  * false if node is not a leaf
  * @node: node to check
  * Return: false if not leaf, true if leaf
@@ -9,8 +9,15 @@
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node == NULL)
-		return 0;
+		return (0);
 
-	return (node->left == NULL && node->right == NULL) ? 1 : 0;
+	if (node->right == NULL && node->left == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
 }
 
